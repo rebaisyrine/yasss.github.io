f27d0d19f3089ac4cb5f65b6ab7b97dd3d01c0fc
unknown <rebaisyrine046@gmail.com>
2024-11-19 00:25:53 +0100

    first commit

---
+++ src/app/ami/ami.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ami',
+  standalone: true,
+  imports: [],
+  templateUrl: './ami.component.html',
+  styleUrl: './ami.component.css'
+})
+export class AmiComponent {
+
+}
